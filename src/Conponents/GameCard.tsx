64a762d08d720c4b32f2bb4673af9a1c93c2718d
ting wei yap <yap.tingwei@gmail.com>
2023-04-10 00:00:54 +0800

    get optimized image by slicing url

--- src/Conponents/GameCard.tsx
+++ src/Conponents/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Box, Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
 import { Game } from '../Hooks/useGames'
 import CriticScore from './CriticScore'
+import getCroppedImageURL from './image-url'
 import PlatformIconList from './PlatformIconList'
 
 
@@ -12,7 +13,7 @@ const GameCard = ( {game} : Props) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
       <Box>
-        <Image src={game.background_image} />
+        <Image src={getCroppedImageURL(game.background_image)} />
         <CardBody>
           <Heading fontSize='2xl'>{game.name}</Heading>
           <HStack justifyContent='space-between'>
