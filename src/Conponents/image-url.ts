cb47874e6d2bf493b717b5dfd06c006ff5ebfb98
ting wei yap <yap.tingwei@gmail.com>
2023-04-10 00:06:07 +0800

    adding notes

--- src/Conponents/image-url.ts
+++ src/Conponents/image-url.ts
@@ -2,7 +2,9 @@ import React from 'react'
 
 const getCroppedImageURL = (url: string) => {
   const target = 'media/'
+  //search for the index where media/ starts, and add its length to get the end index of media/
   const index = url.indexOf(target) + target.length
+  //display the full url up till media/ + cropped + the supposed url after the index
 return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
 
