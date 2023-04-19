5f4f03e3d28142a322029aa0070ef362f74d50c7
ting wei yap <yap.tingwei@gmail.com>
2023-04-19 10:35:28 -0400

    Building search input

--- src/Conponents/ColorModeSwitch.tsx
+++ src/Conponents/ColorModeSwitch.tsx
@@ -1,14 +1,17 @@
-import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
-
   const { toggleColorMode, colorMode } = useColorMode();
   return (
-      <HStack >
-        <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-        <Text>Color Mode</Text>
-      </HStack>
-  )
-}
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text whiteSpace="nowrap">Color Mode</Text>
+    </HStack>
+  );
+};
 
 export default ColorModeSwitch;
--- src/Conponents/navbar.tsx
+++ src/Conponents/navbar.tsx
@@ -1,18 +1,18 @@
-import { HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/MuseHive2.png';
-import ColorModeSwitch from './ColorModeSwitch';
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/MuseHive2.png";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
-
   return (
-    <HStack justify='space-between' padding='12px' height='100px' alignItems='stretch'>
-      <Image src={logo} height='100%' objectFit='cover' />
+    <HStack padding="12px" height="100px" alignItems="center">
+      <Image src={logo} height="100%" objectFit="cover" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
 
 export default NavBar;
-
 
 // 21ead42227eb4cf3aeb7b54aae57fccc
